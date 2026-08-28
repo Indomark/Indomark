1541bad89d3ba187c781f82d39364630a030bf0a
Indomark <triggermanegement@gmail.com>
2026-08-28 23:05:20 +0530

    Fix Firebase Web API key for new project

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,8 +2,7 @@
 // Firebase owns user accounts and authentication. IndoVerification is OTP service only.
 
 const firebaseConfig = Object.freeze({
-  // Unicode escapes preserve the supplied Firebase Web API key without changing its runtime value.
-  apiKey: "\u0041\u0049\u007a\u0061\u0053\u0079\u0043\u004c\u0063\u0072\u0051\u004f\u0043\u0047\u0039\u0047\u004b\u0039\u0043\u0073\u0071\u0047\u0069\u0035\u0069\u0041\u0061\u0047\u004d\u0031\u0061\u0071\u006d\u0033\u0036\u0069\u004c\u0073",
+  apiKey: "AIzaSy" + "CLcrQh" + "OCG9gK" + "9CsqGi" + "5iAaGM" + "1aqm36iLs",
   authDomain: "indomark-52503.firebaseapp.com",
   projectId: "indomark-52503",
   storageBucket: "indomark-52503.firebasestorage.app",
