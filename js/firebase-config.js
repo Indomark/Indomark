fa3eeea7f767632fd0cc78f38312bef557c911ec
Indomark <triggermanegement@gmail.com>
2026-08-28 19:09:49 +0530

    Update Firebase client configuration

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,13 @@
 
 const firebaseConfig = Object.freeze({
   apiKey: "AIzaSyDj5T2zj5GUc4hhkAgbYimLa6ZKlZwnpss",
+  authDomain: "trigger-aa214.firebaseapp.com",
   databaseURL: "https://trigger-aa214-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "trigger-aa214",
   storageBucket: "trigger-aa214.firebasestorage.app",
   messagingSenderId: "633598033396",
-  appId: "1:633598033396:web:0141ccdffee99772a78da1",
-  measurementId: "G-1LZ4V6FT0B"
+  appId: "1:633598033396:web:2e45b99a6b584f5ba78da1",
+  measurementId: "G-540WDMJTQ4"
 });
 
 if (!window.firebase) {
